197ddff2d6f30a00729c88c4737aaabd1ad2ece8
Frank Kloeker <f.kloeker@telekom.de>
2024-04-10 13:27:32 +0200

    fix print clusterid

--- main.go
+++ main.go
@@ -196,7 +196,7 @@ func (c *Config) getNodeCPU() (int, error) {
 	}
 	// node cpu summary
 	for _, node := range nodes.Data {
-		fmt.Println(node.ClusterID
+		fmt.Println(node.ClusterID)
 		//fmt.Println(node.Info.CPU.Count)
 	}
 	//nodeCount := nodes.Data[0].Status.NodeInfo.CPUInfo.NumCores
